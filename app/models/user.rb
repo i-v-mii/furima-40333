bb36330769fd7ea1cc10f7e570fe13ffcd71ff8e
i-v-mii <i.v.jiji.mii@gmail.com>
2024-06-29 02:05:35 +0900

    ログイン、ログアウト機能の実装（一部単体テスト）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
+  validates :password_confirmation, presence: true
+  validate :password_match
+  validates :family_name, presence: true, format: { with: /\A[一-龥ぁ-んー-龥]+\z/, message: 'は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-んー-龥]+\z/, message: 'は全角で入力してください' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :birthday, presence: true
+
+  private
+
+  def password_match
+    errors.add(:password_confirmation, 'が一致しません') if password != password_confirmation
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_24_144611) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "biethday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
