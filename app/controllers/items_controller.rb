89f4d20c8b546413426f8c75a6b9ad658d01f0d1
i-v-mii <i.v.jiji.mii@gmail.com>
2024-06-29 20:59:24 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.all
+  end
+
+  private
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :product_name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :shipping_source_id, presence: true
+  validates :date_of_shipment_id, presence: true
+  validates :explanation, presence: true
+
 end
