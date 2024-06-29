4ee945ad7c9ecfa22ad126a88e1f8048bffffeec
i-v-mii <i.v.jiji.mii@gmail.com>
2024-06-29 14:27:59 +0900

    ユーザー管理機能(修正2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
-  validates :password_confirmation, presence: true
-  validate :password_match
+
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birthday, presence: true
 
-  private
-
-  def password_match
-    errors.add(:password_confirmation, 'が一致しません') if password != password_confirmation
-  end
 end
