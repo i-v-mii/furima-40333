0afa1f7b3dd931a00e8f80f0d6c7b3564134f262
i-v-mii <i.v.jiji.mii@gmail.com>
2024-07-19 15:42:03 +0900

    販売価格の計算の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,19 @@
-window.addEventListener("turbo:load", () => {
-  if ( document.getElementById('item-price')!= null) {
+function setupPriceCalculation() {
+  if (document.getElementById('item-price') != null) {
     const priceInput = document.getElementById("item-price");
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
       const inputValue = document.getElementById("item-price").value;
-        let tax = Math.floor(inputValue * 0.1).toLocaleString(); 
-        addTaxDom.innerHTML = tax ;
-        profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
-    })
+      let tax = Math.floor(inputValue * 0.1);
+      addTaxDom.innerHTML = tax.toLocaleString();
+      let profit = inputValue - tax;
+      profitDom.innerHTML = profit.toLocaleString();
+    });
   }
-});
+}
+
+window.addEventListener("turbo:load", setupPriceCalculation);
+
+document.addEventListener("turbo:render", setupPriceCalculation);
